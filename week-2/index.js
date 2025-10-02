f58a5690c207f307df99bc457139f4697b516467
nyimansata <nyimansataf459@gmail.com>
2025-10-02 20:50:43 +0000

    save week 2 task

---
+++ week-2/index.js
@@ -0,0 +1,27 @@
+// "use strict";
+
+// function NoUseStrict() {
+//   myName = "Nyima";
+// }
+
+// NoUseStrict();
+// console.log(myName);
+
+// function nameConflict(b, b, c) {
+//   return b + b + c;
+// }
+
+// console.log(nameConflict(2, 4, 8));
+
+// let message = "Hello  world";
+// function printMessage() {
+//   let message = "this message is from nyima";
+// }
+
+// printMessage();
+// console.log(message);
+
+// const h1 = document.querySelector("h1");
+const change = (document.querySelector("p").textContent = "Hello from Nyima");
+// alert(h1);
+// prompt("Enter your name?");
